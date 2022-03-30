5d50c42e559381d0663cb868c58f59e67d12f154
yongman Lee <dydaks7878@gmail.com>
2022-03-31 01:06:14 +0900

    시작

---
+++ src/main/java/com/sparta/bepotest/BepotestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bepotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BepotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BepotestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/bepotest/BepotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bepotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BepotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
